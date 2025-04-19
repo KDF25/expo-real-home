6489d77005189c60943fbc28027336293952e613
KDF25 <karimov.damir.faridovich@gmail.com>
2025-04-19 11:19:27 +0500

    fix: added components to the main file

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,16 +1,51 @@
-import { allSettings, SettingsItem } from "@/src/features/settingsList";
+import { allSettings } from "@/src/features/settingsList";
 import { logout } from "@/src/shared/database";
 import { useGlobalContext } from "@/src/shared/providers";
 import { icons } from "@/src/shared/ui";
 import {
   Alert,
   Image,
+  ImageSourcePropType,
   ScrollView,
   Text,
   TouchableOpacity,
-  View
+  View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+interface ISettingsItemProps {
+  icon: ImageSourcePropType;
+  title: string;
+  onPress?: () => void;
+  textStyle?: string;
+  showArrow?: boolean;
+}
+
+export const SettingsItem = ({
+  icon,
+  title,
+  onPress,
+  textStyle,
+  showArrow = true,
+}: ISettingsItemProps) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      className="flex flex-row items-center justify-between py-3"
+    >
+      <View className="flex flex-row items-center gap-3">
+        <Image source={icon} className="size-6" />
+        <Text
+          className={`text-lg font-rubik-medium text-black-300 ${textStyle}`}
+        >
+          {title}
+        </Text>
+      </View>
+
+      {showArrow && <Image source={icons.rightArrow} className="size-5" />}
+    </TouchableOpacity>
+  );
+};
 
 export default function ProfileScreen() {
   const { user, refetch } = useGlobalContext();
